681781bc83770d5a4c7d69e101cbea61ad9242b6
CodingMasterLSW <spqjekdl1004@gmail.com>
2026-04-06 11:21:15 +0900

    chore: project init

---
+++ src/main/java/feedzupzup/sse_module/SseModuleApplication.java
@@ -0,0 +1,13 @@
+package feedzupzup.sse_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseModuleApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SseModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/feedzupzup/sse_module/SseModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package feedzupzup.sse_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
